98773a2dccaaafd8208423826fdc465f91f93e0a
lauramcalpine <lauramcalpine@localhost>
2024-09-11 19:17:53 -0500

    studio2

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,10 +1,33 @@
 package studio2;
 
+import java.util.Scanner;
+
 public class Ruin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is your starting amount of money?");
+		int startAmount = in.nextInt();
+		System.out.println("What is the probability that you win in a single play?");
+		double winChance = in.nextDouble();
+		System.out.println("What amount of money would you be happy leaving with?");
+		int winLimit = in.nextInt();
+		//
+		while (startAmount< winLimit && startAmount>0) {
+			if (Math.random()>winChance) {
+				startAmount--;
+			}
+			else {
+				startAmount++;
+			}
+		
+		}
+		if (startAmount==winLimit)  {
+			System.out.println("You win!");
+		}
+		else
+			System.out.println("You lose!");
+		}
 	}
 
-}
+
